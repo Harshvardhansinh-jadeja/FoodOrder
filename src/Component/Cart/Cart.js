a8cd46705ee5d083b51986c3778f2b6ea7afad2a
Harshvardhansinh Jadeja <harshvardhansinhjadeja49@gmail.com>
2022-07-16 14:41:04 +0530

    Removed Duplicate Items being rendered

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,12 +1,34 @@
 import React from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import {useContext} from "react";
+import cartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(cartContext);
+
+  const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`;
+
+  const HasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{id: "c1", name: "Shushi", amount: 2, price: 12.99}].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        >
+          {item.name}
+        </CartItem>
       ))}
     </ul>
   );
@@ -15,13 +37,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>45.33</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {HasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
